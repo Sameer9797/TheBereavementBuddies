8fb991009359969b75cd94ff92e571285620ba29
Sameer <samahm9797@gmail.com>
2017-10-29 06:03:48 +0000

    make it better

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ require('./config/passport');
 var app = express();
 
 
-  mongoose.connect(process.env.MONGODB);
+ongoose.connect(process.env.MONGODB);
   mongoose.connection.on('error', function() {
     console.log('MongoDB Connection Error. Please make sure that MongoDB is running.');
     process.exit(1);
